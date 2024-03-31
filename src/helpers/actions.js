c15c3776546fa74926c9931e6b23ff14e23a9aba
Viet Ngoc <ngocjohn@me.com>
2024-03-31 03:20:07 +0200

    split files

---
+++ src/helpers/actions.js
@@ -0,0 +1,110 @@
+import { mdiCloseCircle } from '@mdi/js';
+
+export const ActionsHandler = (Superclass) =>
+  class extends Superclass {
+    /* -------------------------------------------------------------------------- */
+    /*                               ACTIONS HANDLER                              */
+    /* -------------------------------------------------------------------------- */
+    scrollToSection(event) {
+      event.preventDefault(); // Prevent default behavior of anchor tag
+      const targetId = event.currentTarget.getAttribute('href'); // Get the hash value
+      const section = this.shadowRoot.querySelector(`${targetId}`); // Get the corresponding section
+      const headerHeight =
+        this.shadowRoot.querySelector('.header').offsetHeight; // Get header height
+      if (section) {
+        // Calculate the target scroll position considering the header height
+        const targetScrollPosition =
+          section.getBoundingClientRect().top + window.scrollY - headerHeight;
+        // Scroll to the target position
+        window.scrollTo({
+          top: targetScrollPosition,
+          behavior: 'smooth',
+        });
+      }
+    }
+
+    handleScroll() {
+      const sections = this.shadowRoot.querySelectorAll('section');
+      const mainElement = this.shadowRoot.querySelector('main');
+      const headerElement = this.shadowRoot.querySelector('.header');
+
+      sections.forEach((section, index) => {
+        const rect = section.getBoundingClientRect();
+
+        // Check if the section is at least partially visible in the viewport
+        const isVisible =
+          rect.bottom > headerElement.offsetHeight &&
+          rect.top <
+            (window.innerHeight || document.documentElement.clientHeight) - 56;
+
+        if (isVisible) {
+          // Change the background color based on odd or even index
+          if (index % 2 === 0) {
+            mainElement.style.backgroundColor = 'var(--primary-color)';
+            headerElement.style.backgroundColor =
+              'var(--app-header-background-color)';
+          } else {
+            mainElement.style.backgroundColor = 'var(--secondary-bg-color)';
+            headerElement.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+          }
+
+          // Exit the loop once a visible section is found
+          return;
+        }
+      });
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                                POPUP DIALOG                                */
+    /* -------------------------------------------------------------------------- */
+
+    // Method to open the more info dialog
+
+    _openPopup(content) {
+      const dialog = this.shadowRoot.querySelector('#popup-dialog');
+
+      if (typeof content === 'string') {
+        // Check if content is a URL for an iframe
+        if (content.startsWith('http') || content.startsWith('www')) {
+          dialog.innerHTML = `
+          <svg class="close"  width="24" height="24" viewBox="0 0 24 24">
+            <path d="${mdiCloseCircle}" />
+          </svg>
+        <div class="popup-content">
+          <iframe src="${content}" frameborder="0" width="100%" height="100%"></iframe>
+        </div>
+      `;
+        } else {
+          // Plain text or HTML string
+          dialog.innerHTML = `
+          <svg class="close"  width="24" height="24" viewBox="0 0 24 24">
+            <path d="${mdiCloseCircle}" />
+          </svg>
+        <div class="popup-content">${content}</div>
+      `;
+        }
+      }
+
+      dialog.showModal();
+      const closeButton = dialog.querySelector('.close');
+      closeButton.addEventListener('click', () => this._closePopup());
+      dialog.addEventListener('click', (event) => {
+        if (event.target === dialog) {
+          this._closePopup();
+        }
+      });
+    }
+
+    _closePopup() {
+      const dialog = this.shadowRoot.querySelector('#popup-dialog');
+      dialog.close();
+    }
+
+    /* ------------------------- PLAY MOVIE CALLSERVICE ------------------------- */
+
+    _playMovie(strm_url) {
+      this._hass.callService('script', 'run_plugin', {
+        file: strm_url,
+      });
+    }
+  };
--- src/movie-panel.js
+++ src/movie-panel.js
@@ -1,15 +1,15 @@
-import { LitElement, html, nothing } from 'lit';
+import { LitElement, html } from 'lit';
 import { styleMap } from 'lit-html/directives/style-map';
-import styles from './styles';
-import mediaquerystyles from './mediaquerystyles';
-import dialogcss from './dialogcss.js';
+import styles from './css/mainstyles.js';
+import mediaquerystyles from './css/mediaquerystyles.js';
+import dialogcss from './css/dialogcss.js';
 
-import { SearchMixin } from './helpers/SearchMixin.js';
-import { mdiCloseCircle } from '@mdi/js';
-import { SEARCH_API, IMG_PATH, API_URL, URL_PATH } from './helpers.js';
-import { loadCSS, noImage } from './const.js';
+import { SearchMixin } from './helpers/search.js';
+import { ActionsHandler } from './helpers/actions.js';
+import { SEARCH_API, IMG_PATH, API_URL, URL_PATH } from './helpers/apiTmdb.js';
+import { loadCSS, noImage } from './helpers/const.js';
 
-export class MovieAppPanel extends SearchMixin(LitElement) {
+export class MovieAppPanel extends ActionsHandler(SearchMixin(LitElement)) {
   _hass;
   static get properties() {
     return {
@@ -274,112 +274,6 @@ export class MovieAppPanel extends SearchMixin(LitElement) {
         <dialog id="popup-dialog"></dialog>
       </main>
     `;
-  }
-
-  /* -------------------------------------------------------------------------- */
-  /*                               ACTIONS HANDLER                              */
-  /* -------------------------------------------------------------------------- */
-
-  scrollToSection(event) {
-    event.preventDefault(); // Prevent default behavior of anchor tag
-    const targetId = event.currentTarget.getAttribute('href'); // Get the hash value
-    const section = this.shadowRoot.querySelector(`${targetId}`); // Get the corresponding section
-    const headerHeight = this.shadowRoot.querySelector('.header').offsetHeight; // Get header height
-    if (section) {
-      // Calculate the target scroll position considering the header height
-      const targetScrollPosition =
-        section.getBoundingClientRect().top + window.scrollY - headerHeight;
-      // Scroll to the target position
-      window.scrollTo({
-        top: targetScrollPosition,
-        behavior: 'smooth',
-      });
-    }
-  }
-
-  handleScroll() {
-    const sections = this.shadowRoot.querySelectorAll('section');
-    const mainElement = this.shadowRoot.querySelector('main');
-    const headerElement = this.shadowRoot.querySelector('.header');
-
-    sections.forEach((section, index) => {
-      const rect = section.getBoundingClientRect();
-
-      // Check if the section is at least partially visible in the viewport
-      const isVisible =
-        rect.bottom > headerElement.offsetHeight &&
-        rect.top <
-          (window.innerHeight || document.documentElement.clientHeight) - 56;
-
-      if (isVisible) {
-        // Change the background color based on odd or even index
-        if (index % 2 === 0) {
-          mainElement.style.backgroundColor = 'var(--primary-color)';
-          headerElement.style.backgroundColor =
-            'var(--app-header-background-color)';
-        } else {
-          mainElement.style.backgroundColor = 'var(--secondary-bg-color)';
-          headerElement.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-        }
-
-        // Exit the loop once a visible section is found
-        return;
-      }
-    });
-  }
-
-  /* -------------------------------------------------------------------------- */
-  /*                                POPUP DIALOG                                */
-  /* -------------------------------------------------------------------------- */
-
-  // Method to open the more info dialog
-
-  _openPopup(content) {
-    const dialog = this.shadowRoot.querySelector('#popup-dialog');
-
-    if (typeof content === 'string') {
-      // Check if content is a URL for an iframe
-      if (content.startsWith('http') || content.startsWith('www')) {
-        dialog.innerHTML = `
-          <svg class="close"  width="24" height="24" viewBox="0 0 24 24">
-            <path d="${mdiCloseCircle}" />
-          </svg>
-        <div class="popup-content">
-          <iframe src="${content}" frameborder="0" width="100%" height="100%"></iframe>
-        </div>
-      `;
-      } else {
-        // Plain text or HTML string
-        dialog.innerHTML = `
-          <svg class="close"  width="24" height="24" viewBox="0 0 24 24">
-            <path d="${mdiCloseCircle}" />
-          </svg>
-        <div class="popup-content">${content}</div>
-      `;
-      }
-    }
-
-    dialog.showModal();
-    const closeButton = dialog.querySelector('.close');
-    closeButton.addEventListener('click', () => this._closePopup());
-    dialog.addEventListener('click', (event) => {
-      if (event.target === dialog) {
-        this._closePopup();
-      }
-    });
-  }
-
-  _closePopup() {
-    const dialog = this.shadowRoot.querySelector('#popup-dialog');
-    dialog.close();
-  }
-
-  /* ------------------------- PLAY MOVIE CALLSERVICE ------------------------- */
-
-  _playMovie(strm_url) {
-    this._hass.callService('script', 'run_plugin', {
-      file: strm_url,
-    });
   }
 
   static getStubConfig() {
